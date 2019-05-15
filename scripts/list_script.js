867ee677ad021c09adaa57aa83c1f29af6ad1e34
Krzysztof Geleta <kgeleta@student.agh.edu.pl>
2019-05-15 12:01:46 +0200

    working version

--- scripts/list_script.js
+++ scripts/list_script.js
@@ -1,16 +1,17 @@
-function generateDynamicTable(){
+
+
+
+function generateDynamicTable() {
 
     var Httpreq = new XMLHttpRequest(); // a new request
-    Httpreq.open("GET","http://localhost:8080/pablito",false);
+    Httpreq.open("GET", "http://localhost:8080/get_top10", false);
     Httpreq.send(null);
     var movies = JSON.parse(Httpreq.responseText)
 
 
-
-
     var noOfMovies = movies.length;
 
-    if(noOfMovies>0){
+    if (noOfMovies > 0) {
         // CREATE DYNAMIC TABLE AND SET ATTRIBUTES.
         var table = document.createElement("table");
         table.style.width = '50%';
@@ -18,9 +19,9 @@ function generateDynamicTable(){
         table.setAttribute('cellspacing', '0');
         table.setAttribute('cellpadding', '5');
         var col = []; // pusta tabela
-        for( var i=0;i< noOfMovies; i++){
-            for (var key in movies[i]){
-                if (col.indexOf(key)=== -1){
+        for (var i = 0; i < noOfMovies; i++) {
+            for (var key in movies[i]) {
+                if (col.indexOf(key) === -1) {
                     col.push(key);
                 }
             }
@@ -30,14 +31,17 @@ function generateDynamicTable(){
 
         var hRow = document.createElement("tr");
 
-        for( var i=0; i< col.length; i++){
-            var th = document.createElement("th");
-            th.innerHTML = col[i];
-            hRow.appendChild(th);
-        }
-        tHead.appendChild(hRow);
-        table.appendChild(tHead);
+        for (var i = 0; i < col.length-1; i++) {
+            if(i>7 && i<12){
 
+            }else {
+                var th = document.createElement("th");
+                th.innerHTML = col[i];
+                hRow.appendChild(th);
+                tHead.appendChild(hRow);
+                table.appendChild(tHead);
+            }
+        }
 
         var tBody = document.createElement("tbody");
 
@@ -46,10 +50,22 @@ function generateDynamicTable(){
             var bRow = document.createElement("tr"); // CREATE ROW FOR EACH RECORD .
 
 
-            for (var j = 0; j < col.length; j++) {
+            for (var j = 0; j < col.length-1; j++) {
                 var td = document.createElement("td");
-                td.innerHTML = movies[i][col[j]];
-                bRow.appendChild(td);
+
+                if(j>7 && j<12){
+                }
+                else if(j===12) {
+                    td.innerHTML = "<img class=\"card-img-top\" src=\"" + movies[i][col[j]] + "\" alt=\"Card image cap\" id=\"img1\">";
+                    bRow.appendChild(td);
+                }
+                else {
+                    td.innerHTML = movies[i][col[j]];
+                    bRow.appendChild(td);
+                }
+
+
+
             }
             tBody.appendChild(bRow)
 
